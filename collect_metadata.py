b2ba065e959469075aaf976e985efe72c441df3b
Nick Young <nick.young@auckland.ac.nz>
2022-04-26 15:36:33 +1200

    calculate number of images per well in collect_metadata.py

--- collect_metadata.py
+++ collect_metadata.py
@@ -16,13 +16,17 @@ for f in tqdm(files):
         slide, machine = match.groups()
         df = pd.read_sql_query("SELECT * from General WHERE Type='Description' AND Par LIKE 'Embryo%'", sqlite3.connect(f))
         df["Well"] = df.Par.str.replace("Embryo", "").astype(int)
+        n_images = pd.read_sql_query("SELECT Well, COUNT(*) AS n_images FROM IMAGES GROUP BY Well", sqlite3.connect(f))
+        df = df.merge(n_images, on="Well", how="left")
+        n_images_F0 = pd.read_sql_query("SELECT Well, COUNT(*) AS n_images_F0 FROM IMAGES WHERE Focal=0 GROUP BY Well", sqlite3.connect(f))
+        df = df.merge(n_images_F0, on="Well", how="left")
         df["FileName"] = filename
         df["Location"] = f
         df["Dataset"] = ""
         df["Machine"] = int(machine)
         df["Slide"] = int(slide)
         df = df.sort_values(by="Well")
-        df = df[["FileName", "Location", "Dataset", "Machine", "Slide", "Well"]]
+        df = df[["FileName", "Location", "Dataset", "Machine", "Slide", "Well", "n_images", "n_images_F0"]]
         result_dfs.append(df)
     except Exception as e:
         print(df)
