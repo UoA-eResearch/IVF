2f7d1db7801df5a765794516cf25fde84303a0c7
Nick Young <nick.young@auckland.ac.nz>
2022-04-29 01:38:14 +0000

    Train a 6 class model by using a softmax activation function. ~96% accuracy

--- predict.py
+++ predict.py
@@ -5,10 +5,21 @@ from tensorflow import keras
 import sys
 import numpy as np
 
+code2class = {
+ 0: '2 Cell',
+ 1: '4 Cell',
+ 2: 'Blastocyst',
+ 3: 'Compacting 8 cell',
+ 4: 'Empty',
+ 5: 'Morula'
+}
+
 image = keras.utils.load_img(sys.argv[1], target_size=(299,299))
 input_arr = keras.preprocessing.image.img_to_array(image)
 input_arr = input_arr / 255
 input_arr = np.array([input_arr])  # Convert single image to a batch.
 
-model = keras.models.load_model('models/empty_or_not.h5')
-print(model.predict(input_arr).argmax(axis=1)[0])
+model = keras.models.load_model('models/6_class.h5')
+
+prediction = model.predict(input_arr).argmax(axis=1)[0]
+print(code2class[prediction])
